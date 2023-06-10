aad96f4e55a6ee23710531cc2d269f9154ab98ff
MajidRaimi <imajid2000z1@gmail.com>
2023-06-10 12:17:59 +0300

    Tech ✅

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,8 +1,23 @@
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+import { fadeIn } from '../animations';
+import { motion } from 'framer-motion';
 
+// eslint-disable-next-line react-refresh/only-export-components
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10 items-center text-center'>
+      {
+        technologies.map((tech, index) => (
+          <motion.div key={tech.name} className='w-28 h-28 mx-auto' variants={fadeIn('right', '', 1, index*0.05)}>
+            <BallCanvas icon={tech.icon} />
+          </motion.div>
+        ))
+      }
+    </div>
   )
 }
 
-export default Tech
+// eslint-disable-next-line react-refresh/only-export-components
+export default SectionWrapper(Tech, '')
